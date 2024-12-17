dd8afe9b507b8d6bf5e3553c5c59cd21b39cf19b
Efim Parshin <xefimix@gmail.com>
2024-12-17 21:39:00 +0100

    fix: challenge description

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,13 +1,13 @@
 sap.ui.define(
   [
-    "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/core/Fragment",
+    'sap/ui/core/mvc/Controller',
+    'sap/ui/model/json/JSONModel',
+    'sap/ui/core/Fragment',
   ],
   (Controller, JSONModel, Fragment) => {
-    "use strict";
+    'use strict';
 
-    return Controller.extend("dh.fs.wschallenge.controller.Main", {
+    return Controller.extend('dh.fs.wschallenge.controller.Main', {
       onInit() {
         this.oModel = new JSONModel({
           items: [],
@@ -18,15 +18,16 @@ sap.ui.define(
 
       async setModelData() {
         //Instead of hardcoded object, data should be fetched from https://api.imgflip.com/get_memes
-        // const data = [
-        //   {
-        //     name: "Chill guy",
-        //     url: "https://i.imgflip.com/9au02y.jpg",
-        //     captions: 29750,
-        //   },
-        // ];
-        const { data } = await jQuery.ajax("https://api.imgflip.com/get_memes");
-        this.oModel.setProperty("/items", data.memes);
+        const data = [
+          {
+            name: 'Chill guy',
+            url: 'https://i.imgflip.com/9au02y.jpg',
+            captions: 29750,
+          },
+        ];
+        this.oModel.setProperty('/items', data);
+
+        //!!!extra challenge to add new column to Main.view.xml and display the position of the meme in the list!!!
       },
 
       async openPreview(oEvent) {
@@ -34,7 +35,7 @@ sap.ui.define(
 
         if (!this._oPreviewDialog) {
           this._oPreviewDialog = await Fragment.load({
-            name: "dh.fs.wschallenge.view.fragments.ImagePreviewDialog",
+            name: 'dh.fs.wschallenge.view.fragments.ImagePreviewDialog',
             controller: this,
           });
           this.getView().addDependent(this._oPreviewDialog);
